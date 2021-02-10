19441077bb7cad1c1630dae5f941c8c8dcd580af
Drew Weston <awfiscus@comcast.net>
2021-02-10 09:46:06 -0700

    added links to linked in page and github, as well as stylized profile page and navbars for all three html pages

---
+++ script.js
@@ -0,0 +1,11 @@
+var email = $("#email")
+var github = $("#github")
+var linkedin = $("#linkedin")
+
+github.on("click", function () {
+    window.open("https://github.com/Awfiscus", "_blank")
+})
+
+linkedin.on("click", function () {
+    window.open("https://www.linkedin.com/in/andrew-fiscus-1bb3a4206/", "_blank")
+})
