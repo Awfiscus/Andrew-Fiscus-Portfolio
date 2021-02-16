783563179c81f96e0f1f98098841b107c128c1f2
Drew Weston <awfiscus@comcast.net>
2021-02-15 19:19:48 -0700

    added projects to portforlio page as well changed some css settings to make it more responsive

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-var email = $("#email")
-var github = $("#github")
-var linkedin = $("#linkedin")
+
+const github = $("#github")
+const linkedin = $("#linkedin")
+const project1 = $("#smileBtn")
+const project2 = $("#weatherBtn")
+const project3 = $("#workdayBtn")
 
 github.on("click", function () {
     window.open("https://github.com/Awfiscus", "_blank")
@@ -8,4 +11,16 @@ github.on("click", function () {
 
 linkedin.on("click", function () {
     window.open("https://www.linkedin.com/in/andrew-fiscus-1bb3a4206/", "_blank")
+})
+
+project1.on("click", function () {
+    window.open("https://lukeoxner.github.io/project-1/", "_blank")
+})
+
+project2.on("click", function () {
+    window.open("https://awfiscus.github.io/travelers-best-friend-weather-report/", "_blank")
+})
+
+project3.on("click", function () {
+    window.open("https://awfiscus.github.io/keep-your-workday-organized/", "_blank")
 })
